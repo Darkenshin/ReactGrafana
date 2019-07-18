8c474ee1eb56882a5c56753d26442b38765809fb
unknown <taran.f@hotmail.fr>
2019-07-18 13:43:02 -0400

    ttt

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import DashBordListView from "./pages/DashBordListView";
+import InputJsonData from "./pages/InputJsonData";
 
-function App() {
+const App = () => {
+  const [dataInput, setDataInput] = useState("");
+  const [dataDashBord, setDataDashBord] = useState("");
+
+  const handleSubmit = () => {
+    const name = Array.from(
+      document
+        .getElementById("root_check")
+        .getElementsByClassName("form-check-input")
+    );
+    name.forEach(element => {
+      if (element.checked) {
+        getData(element.getAttribute("uid"));
+        console.log(dataDashBord);
+        // sendData(element.getAttribute("uid"));
+      }
+    });
+  };
+
+  const getData = async uid => {
+    let response = await fetch("/api/dashboards/uid/" + uid, {
+      credentials: "include",
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": " application/json",
+        Authorization:
+          "Bearer eyJrIjoiRDhHb2NLYmljdmNsY2hCMDE0a1BWS3NNanB5MEpGN0UiLCJuIjoidGVzdCIsImlkIjoxfQ=="
+      }
+    });
+    let response2 = await Promise.resolve(response).then(data => {
+      return data;
+    });
+    let response3 = response.json();
+    return response3;
+  };
+  const createBody = element => {
+    let data = JSON.parse(dataInput);
+    data.Dashboard.uid = element;
+    return JSON.stringify(data);
+  };
+  const sendData = element => {
+    let body = createBody(element);
+    console.log(body);
+    fetch("http://localhost:3000/api/dashboards/db", {
+      credentials: "include",
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": " application/json",
+        Authorization:
+          "Bearer eyJrIjoiRDhHb2NLYmljdmNsY2hCMDE0a1BWS3NNanB5MEpGN0UiLCJuIjoidGVzdCIsImlkIjoxfQ=="
+      },
+      body: body
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <InputJsonData setDataInput={setDataInput} />
+      <DashBordListView />
+      <input type="submit" onClick={handleSubmit} />
     </div>
   );
-}
+};
 
 export default App;
